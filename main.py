799751ba3aee5507787e436c55d09f63348acd9d
orion7 <toribios254@gmail.com>
2025-08-29 08:54:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyscript import display
+
+restaurant_name = "Sunshine & Sweets" #string data type
+owner_name = "Sasha Giulia P. Toribio" #string data type
+year_established = "Since..." #string data type
+year_established1 = 2025 #integer data type
+has_delivery = True #Boolean type
+business_hours = "5AM to 7AM & 3PM to 6PM" 
+tax_rate = 0.12 #Floating-point data type
+common_allergies = set[('Eggs', 'milk')] #set data type
+popular_item_price = ['₱80','₱50', '₱30', '₱100', '₱100'] #list data type
+product_name = ['Leche Flan', 'Halo-halo', 'Matcha Scramble', 'Banana Milkshake', 'Mango Milkshake'] #list data type
+
+display(restaurant_name)
+display(owner_name)
+display(year_established) 
+display(year_established1)
+display(business_hours)
